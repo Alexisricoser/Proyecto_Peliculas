593a7920ba8d19d21c2600214c0bfb59c72f388f
suky-sk <sokaynaartana@gmail.com>
2025-10-24 15:49:55 +0200

    Delete Benkari_sokayna_task1 directory

--- Benkari_sokayna_task1/Benkari_Masoudi_Sokayna_T1Practica/p2/script.js
+++
@@ -1,8 +0,0 @@
-            const numeros = [6, 8, 3, 12, 18];
-            let suma = 0;
-            for (let i = 0; i < numeros.length; i++) {
-                if (numeros[i] > 8) {
-                    suma += numeros[i];
-                }
-            }
-            alert("La suma de los números mayores a 8 es: " + suma);
--- Benkari_sokayna_task1/Benkari_Masoudi_sokayna_T1Actividad1/alertas.js
+++
@@ -1,9 +0,0 @@
-        //El primer mensaje debe mostrar el mensaje de alerta “Hola mundo”.
-        alert("Hola mundo");
-
-        //El segundo mensaje debe mostrar una constante que se haya definido con vuestro nombre completo (Nombre + Apellido1 + Apellido2).
-        const alumna = "Sokayna Benkari Masoudi";
-        alert(alumna);
-
-        //El tercer mensaje debe mostrar que el comando alert es realmente una función específica de javascript.
-        alert(typeof alert);
